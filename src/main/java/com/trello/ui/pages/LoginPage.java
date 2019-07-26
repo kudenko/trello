5829526e7ab9b68867403879f509a2478f3379aa
s.kudenko <ky_se_se@mail.ru>
2019-07-27 02:35:02 +0300

    try fix xml

--- src/main/java/com/trello/ui/pages/LoginPage.java
+++ src/main/java/com/trello/ui/pages/LoginPage.java
@@ -3,6 +3,8 @@ package com.trello.ui.pages;
 import com.trello.ui.core.Constant;
 import com.trello.ui.core.Elem;
 import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 
 import static com.trello.ui.core.BrowserFactory.*;
@@ -10,13 +12,16 @@ import static com.trello.ui.core.BrowserFactory.*;
 
 public class LoginPage {
     private static String PATH = "/login";
+    Logger logger = LoggerFactory.getLogger(getClass());
 
     public Elem emailFld = new Elem(By.cssSelector("#user"), "Login field");
     public Elem passFld = new Elem(By.cssSelector("#password"), "Pass field");
     public Elem loginBtn = new Elem(By.cssSelector("#login"), "Login button");
 
     public void open(){
+
         get(Constant.URL+PATH);
+        logger.info("get " + Constant.URL+PATH);
         Assert.assertTrue(isOpened(), "Page 'Login' ["+PATH+"] not Opened");
     }
 
--- src/test/java/regression/LoginTest.java
+++ src/test/java/regression/LoginTest.java
@@ -6,9 +6,12 @@ import com.trello.ui.pages.LoginPage;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Step;
 import io.qameta.allure.Story;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BrowserFactory {
+    Logger logger = LoggerFactory.getLogger(getClass());
     LoginPage loginPage = new LoginPage();
     BoardsPage boardsPage = new BoardsPage();
 
@@ -18,7 +21,10 @@ public class LoginTest extends BrowserFactory {
     @Step
     public void login() {
         loginPage.open();
+        logger.info("Enter pass and email");
         loginPage.login("jubekamefa@first-mail.info", "312312312a");
+        logger.info("Open dash");
+
         boardsPage.openBoard("/b/Ad39BPpI/first-table");
     }
 }
